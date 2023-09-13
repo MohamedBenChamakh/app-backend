52eaa4b37548a06b6c4de6eb3369d615f894ecfd
MohamedBenChamakh <mohamed.ben.chamakh98@gmail.com>
2023-09-13 14:52:07 +0100

    add swagger and open api

--- src/main/java/com/example/appbackend/AppBackendApplication.java
+++ src/main/java/com/example/appbackend/AppBackendApplication.java
@@ -2,8 +2,10 @@ package com.example.appbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class AppBackendApplication {
 
 	public static void main(String[] args) {
